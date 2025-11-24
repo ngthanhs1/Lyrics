773435b062347514b35da40b1b12bbaf129ae43c
ngthanhs1 <thanhtuyen191205@gmail.com>
2025-11-24 15:45:02 +0700

    bbb

--- sommuonthi.py
+++ sommuonthi.py
@@ -13,17 +13,17 @@ def animate_text(text, speed=0.1):
 
 def sing_song():
     lyrics = [
-        ("🎧Baby hỏi khi nào về", 0.108, 0.5),
-        ("Anh tung ngay đồng xu", 0.148, 1.2),
-        ("Chỉ là 50/50, anh biết thế ", 0.09, 3.0),
-        ("Nên anh sẽ phải chiến thắng(uh), dù nếm cay đắng(uh)", 0.12, 3.7),
-        ("Bay lên cao vút, mang cả nắng về", 0.12, 4.0),
-        ("Để thắp sáng trái tim, mặc cho đời nhấn chìm", 0.04, 1.0),
-        ("Tình yêu này ngoi lên như vì sao băng trong đêm", 0.176, 8.1),
-        ("Sao băng trong đêm...", 0.1, 16.1)
+        ("🎧Baby hỏi khi nào về", 0.4, 0.1),
+        ("Anh tung ngay đồng xu", 1.0, 0.1),
+        ("Chỉ là 50/50, anh biết thế thoi", 1.4, 0.1),
+        ("Nên anh sẽ phải chiến thắng(uh), dù nếm cay đắng(uh)", 0.007, 0.06),
+        ("Bay lên cao vút, mang cả nắng về", 0.004, 0.08),
+        ("Để thắp sáng trái tim, mặc cho đời nhấn chìm", 0.009, 0.07),
+        ("Tình yêu này ngoi lên như vì sao băng trong đêm", 0.001, 0.05),
+        ("Sao băng trong đêm...", 0.009, 0.057)
     ]
     t0 = time.perf_counter()
-    for text, speed, start_at in lyrics:
+    for text, start_at, speed in lyrics:
         now = time.perf_counter() - t0
         wait = start_at - now
         if wait > 0:
